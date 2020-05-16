805b1382ca0f6bf59786f3c70a88773b36f16ef7
MateNaw <mateusz.nawrat@wp.pl>
2020-05-16 12:28:00 +0200

    fix typo and add some frontend

---
+++ Backend/src/main/java/com/scrapper/country/CountryService.java
@@ -0,0 +1,26 @@
+package com.scrapper.country;
+
+import com.scrapper.Continect.Continent;
+import com.scrapper.country.model.Country;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public interface CountryService {
+
+    Optional<Country> getCountryById(Integer countryId) throws IllegalArgumentException;
+
+    Optional<Country> getCountryByName(String name) throws IllegalArgumentException;
+
+    Optional<Collection<Country>> getCountryByContinent(Continent continent) throws IllegalArgumentException;
+
+    Collection<Country> getAllCountries();
+
+    Country addCountry(Country country) throws IllegalArgumentException;
+
+    Country addOrCreateCountry(Country country) throws IllegalArgumentException;
+
+    void deleteCountryById(Integer countryId)  throws IllegalArgumentException;
+}
